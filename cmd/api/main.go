4c2431c1d49d15219bd31fb9a3e331a1250ed177
Faculdade Embu das Artes <90737370+JeanPDR@users.noreply.github.com>
2026-03-11 21:30:43 -0300

    adicionando cors na api

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,6 +7,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/rs/cors"
 )
 
 func main() {
@@ -21,27 +23,43 @@ func main() {
 	authRepo := auth.NewRepository(db)
 	authHandler := auth.NewHandler(authRepo, mailSvc)
 
-	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "API Online 2.0 🚀")
 	})
 
-	http.HandleFunc("/register", authHandler.RegisterUser)
-	http.HandleFunc("/login", authHandler.LoginUser)
-	http.HandleFunc("/refresh", authHandler.RefreshToken)
-	http.HandleFunc("/verify", authHandler.VerifyEmail)
-	http.HandleFunc("/verify/resend", authHandler.ResendVerificationCode)
-	http.HandleFunc("/forgot-password", authHandler.ForgotPassword)
-	http.HandleFunc("/reset-password", authHandler.ResetPassword)
+	mux.HandleFunc("/register", authHandler.RegisterUser)
+	mux.HandleFunc("/login", authHandler.LoginUser)
+	mux.HandleFunc("/refresh", authHandler.RefreshToken)
+	mux.HandleFunc("/verify", authHandler.VerifyEmail)
+	mux.HandleFunc("/verify/resend", authHandler.ResendVerificationCode)
+	mux.HandleFunc("/forgot-password", authHandler.ForgotPassword)
+	mux.HandleFunc("/reset-password", authHandler.ResetPassword)
 
-	http.HandleFunc("/dashboard", auth.AuthMiddleware(func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/dashboard", auth.AuthMiddleware(func(w http.ResponseWriter, r *http.Request) {
 		userID := r.Context().Value(auth.UserIDKey).(string)
 
 		w.Header().Set("Content-Type", "application/json")
 		fmt.Fprintf(w, `{"message": "Bem-vindo ao sistema protegido!", "seu_id": "%s"}`, userID)
 	}))
 
-	fmt.Println("Servidor rodando na porta :8080")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{
+			"http://localhost:4200",         
+			"https://app.seudominio.com.br", 
+		},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		AllowCredentials: true,
+		Debug:            false, 
+	})
+
+	handlerComCors := c.Handler(mux)
+
+	fmt.Println("Servidor rodando na porta :8080 com CORS ativado!")
+	if err := http.ListenAndServe(":8080", handlerComCors); err != nil {
 		log.Fatal(err)
 	}
 }
